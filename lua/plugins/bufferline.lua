163e60d481baf7d58c41c960a66599c20028c383
MaximGaranin <lmibuh3@gmail.com>
2026-04-26 21:45:18 +0400

    fix: wrap bufferline in lazy return {}

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,3 +1,10 @@
-
-vim.opt.termguicolors = true
-require("bufferline").setup{}
+return {
+  {
+    'akinsho/bufferline.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      vim.opt.termguicolors = true
+      require('bufferline').setup({})
+    end,
+  },
+}
