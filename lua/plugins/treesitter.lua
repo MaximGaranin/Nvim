cca632cc425a6202d7211c2344418d93978d2660
MaximGaranin <lmibuh3@gmail.com>
2026-04-26 21:45:26 +0400

    fix: wrap treesitter in lazy return {}

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,18 @@
-
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-	"bash",
-	"css",
-	"dockerfile",
-	"html",
-	"javascript",
-	"json",
-	"json5",
-	"lua",
-	"python",
-	"vim",
-	"yaml",
-	"c",
-	"go",
-	"rust",
-	},
-
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'bash', 'css', 'dockerfile', 'html', 'javascript',
+          'json', 'json5', 'lua', 'python', 'vim',
+          'yaml', 'c', 'go', 'rust',
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true },
+      })
+    end,
   },
 }
