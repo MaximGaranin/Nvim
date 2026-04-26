9961d74d469ad3c24ea536f8d69058e7b024e667
MaximGaranin <lmibuh3@yandex.ru>
2026-04-26 21:37:29 +0400

    Fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,72 +1,50 @@
-local cmp = require('cmp')
-
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            -- Используйте один из вариантов в зависимости от вашего сниппет-плагина
-            vim.snippet.expand(args.body)
-            -- или другие варианты
-        end,
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({select = true}),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
-            end
-        end, {"i", "s"})
-    }),
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'},
-    }, {
-        {name = 'buffer'},
-        {name = 'nvim_lsp_signature_help'}
-    })
-})
-
--- Set configuration for specific filetype
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        {name = 'cmp_git'}
-    }, {
-        {name = 'buffer'}
-    })
-})
-
--- Use buffer source for `/` and `?`
-cmp.setup.cmdline({'/', '?'}, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {{name = 'buffer'}}
-})
-
--- Use cmdline & path source for `:`
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { 
-      name = 'cmdline',
-      option = {
-        ignore_cmds = { 'Man', '!' }
-      }
-    }
-  })
-})
+return {
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-cmdline' },
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          expand = function(args) vim.snippet.expand(args.body) end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item() else fallback() end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item() else fallback() end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+        }, {
+          { name = 'buffer' },
+          { name = 'nvim_lsp_signature_help' },
+        }),
+      })
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = 'buffer' } },
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({ { name = 'path' } }, {
+          { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } },
+        }),
+      })
+    end,
+  },
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,16 @@
-
-require("mason").setup({
-    ui = {
-        icons = {
+return {
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
             package_installed = "✓",
             package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+}
