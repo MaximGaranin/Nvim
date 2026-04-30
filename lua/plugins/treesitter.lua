c9756635c1330847d5277f04c95762cc36d6e810
MaximGaranin <lmibuh3@gmail.com>
2026-04-30 13:30:06 +0400

    fix: migrate treesitter config to new API (nvim-treesitter v1.0+)
    Replace deprecated require('nvim-treesitter.configs').setup() with
    main = 'nvim-treesitter.configs' + opts pattern for lazy.nvim.
    
    Fixes: module 'nvim-treesitter.configs' not found error.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,17 +2,16 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          'bash', 'css', 'dockerfile', 'html', 'javascript',
-          'json', 'json5', 'lua', 'python', 'vim',
-          'yaml', 'c', 'go', 'rust',
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-      })
-    end,
+    main = 'nvim-treesitter.configs',
+    opts = {
+      ensure_installed = {
+        'bash', 'css', 'dockerfile', 'html', 'javascript',
+        'json', 'json5', 'lua', 'python', 'vim',
+        'yaml', 'c', 'go', 'rust',
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+    },
   },
 }
